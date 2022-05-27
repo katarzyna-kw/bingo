c112e3cd508e8bffb6637508f5a0f8e73a8d33fe
katarzyna-kw <katarzyna.koch@gmail.com>
2022-05-27 10:30:15 -0500

    script to generate random bingo card pdf

--- bingo.rb
+++ bingo.rb
@@ -1,9 +1,5 @@
-# B = 1 through 15
-# I = 16 through 30
-# N = 31 through 45
-# G = 46 through 60
-# O = 61 through 75
-# 5×5 grid
+require "prawn"
+
 
 b_col = (1..15).to_a.sample(5)
 i_col = (16..30).to_a.sample(5)
@@ -11,30 +7,81 @@ n_col = (31..45).to_a.sample(4)
 g_col = (46..60).to_a.sample(5)
 o_col = (61..75).to_a.sample(5)
 
-# puts b_col.inspect
-# puts i_col.inspect
-# puts n_col.inspect
-# puts g_col.inspect
-# puts o_col.inspect
 
-# list.each do |item|
-#   puts item.inner_text
-# end
+def align(num)
+  if num < 10
+    "  #{num}"
+  else
+    " #{num}"
+  end
+end
+
 
 bingo_card = <<-BINGO
-+---+---+---+---+---+
-| B | I | N | G | O |
-+---+---+---+---+---+
-| #{b_col[0]} |#{i_col[0]} |#{n_col[0]} |#{g_col[0]} |#{o_col[0]} |
-+---+---+---+---+---+
-| #{b_col[1]} |#{i_col[1]} |#{n_col[1]} |#{g_col[1]} |#{o_col[1]} |
-+---+---+---+---+---+
-| #{b_col[2]} |#{i_col[2]} |F |#{g_col[2]} |#{o_col[2]} |
-+---+---+---+---+---+
-| #{b_col[3]} |#{i_col[3]} |#{n_col[3]} |#{g_col[3]} |#{o_col[3]} |
-+---+---+---+---+---+
-| #{b_col[4]} |#{i_col[4]} |#{n_col[4]} |#{g_col[4]} |#{o_col[4]} |
-+---+---+---+---+---+
++-----+-----+-----+-----+-----+
+|  B  |  I  |  N  |  G  |  O  |
++-----+-----+-----+-----+-----+
+| #{align(b_col[0])} | #{align(i_col[0])} | #{align(n_col[0])} | #{align(g_col[0])} | #{align(o_col[0])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[1])} | #{align(i_col[1])} | #{align(n_col[1])} | #{align(g_col[1])} | #{align(o_col[1])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[2])} | #{align(i_col[2])} |  F  | #{align(g_col[2])} | #{align(o_col[2])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[3])} | #{align(i_col[3])} | #{align(n_col[2])} | #{align(g_col[3])} | #{align(o_col[3])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[4])} | #{align(i_col[4])} | #{align(n_col[3])} | #{align(g_col[4])} | #{align(o_col[4])} |
++-----+-----+-----+-----+-----+
 BINGO
- 
+
 puts bingo_card
+
+bingo_letters = ["B", "I", "N", "G", "O"]
+n_col.insert(2, "F")
+
+
+Prawn::Document.generate("bingo.pdf") do
+  define_grid(columns: 5, rows: 6)
+
+  bingo_letters.each_with_index do |letter, i|
+    grid(0, i).bounding_box do
+      stroke_bounds
+      text letter, align: :center, valign: :center, size: 50, style: :bold
+    end
+  end
+
+  b_col.each_with_index do |num, i|
+    grid((i + 1), 0).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  i_col.each_with_index do |num, i|
+    grid((i + 1), 1).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  n_col.each_with_index do |num, i|
+    grid((i + 1), 2).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  g_col.each_with_index do |num, i|
+    grid((i + 1), 3).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  o_col.each_with_index do |num, i|
+    grid((i + 1), 4).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+end
